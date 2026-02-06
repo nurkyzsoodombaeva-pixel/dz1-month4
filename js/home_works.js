3d5c93357b5b3006a7e71e2819b6d8317d8db2cb
Nurkyz <nurkyzsoodombaeva@icloud.com>
2026-02-06 20:01:44 +0600

    dz4

--- js/home_works.js
+++ js/home_works.js
@@ -17,9 +17,6 @@ gmail_button.addEventListener('click', () => {
 const parent_block = document.querySelector('.parent_block');
 const child_block = document.querySelector('.child_block');
 
-console.log(parent_block);
-console.log(child_block);
-
 let positionX = 0;
 let positionY = 0;
 let directionX = 0;
@@ -83,4 +80,33 @@ resetBtn.addEventListener('click', () => {
     second = 0;
     seconds.innerHTML = second;
 })
+const charactersContainer = document.querySelector('.characters-list')
 
+const requester = new XMLHttpRequest()
+requester.open('GET', '../data/characters.json')
+requester.setRequestHeader('Content-type', 'application/json')
+requester.send()
+requester.onload = () => {
+    const data = JSON.parse(requester.response)
+    data.map((character)=> {
+        const card = document.createElement('div')
+        card.classList.add('character-card')
+        card.innerHTML = `
+             <h2>${character.name}</h2>
+    <div class="character-photo"><img src="${character.photo}" alt="${character.name}"></div>
+   
+    <p style="color:white">${character.age}</p>
+        `
+    charactersContainer.appendChild(card)
+    })
+}
+
+
+const qwerty = new XMLHttpRequest()
+qwerty.open('GET', '../data/bio.json')
+qwerty.setRequestHeader('Content-type', 'application/json')
+qwerty.send()
+qwerty.onload = () => {
+    const data = JSON.parse(qwerty.response)
+    console.log(data);
+}

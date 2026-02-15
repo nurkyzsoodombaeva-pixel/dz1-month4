6f44d1678187b19b5d3a858c170ba3c92cc6c8f5
Nurkyz <nurkyzsoodombaeva@icloud.com>
2026-02-15 14:44:36 +0600

    dz6

--- js/lesson.js
+++ js/lesson.js
@@ -122,3 +122,88 @@ converter(eurInput, somInput, usdInput);
 //         somInput.value = (usdInput.value * data.usd).toFixed(2);
 //     };
 // }
+
+const btnNext = document.querySelector('#btn-next');
+const btnPrev = document.querySelector("#btn-prev")
+const card = document.querySelector('.card');
+
+const API_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+let todoId = 1;
+
+const getTodo = () => {
+    fetch(`${API_URL}/${todoId}`)
+        .then(response => response.json())
+        .then(({id, title, completed}) => {
+            let color = completed ? 'green' : 'red';
+            card.style.borderColor = color;
+            card.innerHTML = `<p>ID => ${id}</p>
+            <p>${title}</p>
+            <p>${completed}</p>
+            <p style="color: ${color}">${completed}</p>`
+        })
+}
+
+getTodo()
+
+btnNext.addEventListener('click', () => {
+    todoId++
+    if (todoId > 200) {
+        todoId = 1
+    }
+    getTodo()
+})
+
+btnPrev.addEventListener('click', () => {
+    todoId--
+    if (todoId < 1) 
+        todoId = 200
+    getTodo()
+})
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(post => console.log({title: post.title, body: post.body}))
+    })
+
+
+
+
+// const btnNext1 = document.querySelector("#btn-next1")
+// const btnPrev1 = document.querySelector("#btn-prev1")
+// const card1 = document.querySelector(".card1")
+// let cardId = 1
+
+// function getData(id) {
+//     fetch(`https://jsonplaceholder.typicode.com/todos/${cardId}`)
+//         .then(res => res.json())
+//         .then(data => {
+//             const {title, id,completed} = data;
+//             card.innerHTML = `
+//             <p>${title}</p>
+//             <p>${completed}</p>
+//             <span> ${id}</span>
+//             <p style="borderColor: ${completed ? 'green' : 'red'}">Status: ${completed ? 'Completed' : 'Not Completed'}</p>
+//             `
+//         })
+// }
+// btnNext.onclick = () => {
+//     cardId = cardId >= 200 ? 1 : cardId +1;
+//     getData(cardId)
+// }
+
+// btnPrev.onclick = () => {
+//     cardId = cardId <= 1 ? 200 : cardId - 1;
+//     getData(cardId)
+// }
+// getData()
+
+// function getPosts() {
+//     fetch(`https://jsonplaceholder.typicode.com/posts`)
+//         .then(res => res.json())
+//         .then(data => {
+//         console.log(data)
+//     })
+// }
+// getPosts()
